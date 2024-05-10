8cd0a2a342c1f507a683229627882058442ab9e7
hj <hawking2215@gmail.com>
2024-05-10 08:28:23 +0800

    update

--- svg.go
+++ svg.go
@@ -5,8 +5,8 @@ package svg
 // #include "svg_api.h"
 // #cgo CFLAGS: -I ./  -I ./libs
 // #cgo CXXFLAGS: -I ./ -I ./libs
-// #cgo linux LDFLAGS:  -L ./libs/linux -Wl,--start-group  -lstdc++ -lm -pthread -ldl -lcsvg -lsvg -lxml2 -lzlib -lantigrain -lcontrols -lexpat -lfontconfig -lfreetype -licudata -licudt -licui18n -licutools -licuuc   -Wl,--end-group
-// #cgo windows LDFLAGS: -L ./libs/windows -lcsvg -lsvg -lxml2 -lzlib -lantigrain -lcontrols -lexpat -lfontconfig -lfreetype -licudata -licudt -licui18n -licutools -licuuc
-// #cgo darwin LDFLAGS: -L /usr/libs/darwin -lc++ -L　./libs -lcsvg -lsvg -lxml2 -lzlib -lantigrain -lcontrols -lexpat -lfontconfig -lfreetype -licudata -licudt -licui18n -licutools -licuuc
-// #cgo darwin,arm LDFLAGS: -L /usr/libs/darwin_arm -lc++ -L　./libs -lcsvg -lsvg -lxml2 -lzlib -lantigrain -lcontrols -lexpat -lfontconfig -lfreetype -licudata -licudt -licui18n -licutools -licuuc
+// #cgo linux LDFLAGS:  -L ./libs/linux -Wl,--start-group  -lstdc++ -lm -pthread -ldl -lcsvg -lsvg -lxml2 -lzlib -lantigrain -lcontrols -lexpat -lfontconfig -lfreetype -licutools-svg -licui18n-svg -licudt-svg -licudata-svg -licuuc-svg   -Wl,--end-group
+// #cgo windows LDFLAGS: -L ./libs/windows -lcsvg -lsvg -lxml2 -lzlib -lantigrain -lcontrols -lexpat -lfontconfig -lfreetype -licutools-svg -licui18n-svg -licudt-svg -licudata-svg -licuuc-svg
+// #cgo darwin LDFLAGS: -L /usr/libs/darwin -lc++ -L　./libs -lcsvg -lsvg -lxml2 -lzlib -lantigrain -lcontrols -lexpat -lfontconfig -lfreetype -licutools-svg -licui18n-svg -licudt-svg -licudata-svg -licuuc-svg
+// #cgo darwin,arm LDFLAGS: -L /usr/libs/darwin_arm -lc++ -L　./libs -lcsvg -lsvg -lxml2 -lzlib -lantigrain -lcontrols -lexpat -lfontconfig -lfreetype -licutools-svg -licui18n-svg -licudt-svg -licudata-svg -licuuc-svg
 import "C"
